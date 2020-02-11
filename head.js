0c84fb2de948ce6a109ca87c24bdef6f420201fc
oceanway1 <oceanway1@yahoo.ca>
2020-02-11 23:28:22 +0000

    Implement tail, function returns every element except the first element of the array

--- head.js
+++ head.js
@@ -11,6 +11,6 @@ const head = function (arr) {
   return arr[0]
 }
 
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]))
